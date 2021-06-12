14978f69b01ba9f05bcdd4156203ffee2d5695fa
whatsaxis <59597828+whatsaxis@users.noreply.github.com>
2021-06-12 17:58:37 +0300

    Update settings.py

--- project_name/settings.py
+++ project_name/settings.py
@@ -87,7 +87,6 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'accounts.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
