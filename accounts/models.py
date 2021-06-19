7b3146047101a833d0e81ebf699b50216cabefce
whatsaxis <59597828+whatsaxis@users.noreply.github.com>
2021-06-19 16:44:16 +0300

    Delete accounts directory

--- accounts/models.py
+++
@@ -1,1 +0,0 @@
-# Implement custom User model here, if necessary.
--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import SignUpView
-
-
-app_name = 'accounts'
-urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-]
